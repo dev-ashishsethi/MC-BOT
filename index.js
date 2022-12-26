3dcce12ca7e3ae27b6d02915fbcf403a64d993f7
Ashish Sethi <me.ashishsethi@gmail.com>
2022-12-26 19:52:57 +0530

    fix: port number

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ client.on('messageCreate', async (message)=>{
     }
 
     if(command==='status'){
-        ping.status(process.env.URL,Number(process.env.PORT),options).then(res=>message.channel.send(`MC server version = ${res.version.name}, number of players online = ${res.players.online} and latency = ${res.roundTripLatency}`)).catch(err=>message.channel.send(`Sorry, couldn't connect to the server. Here's the error I got ${err} `))
+        ping.status(process.env.URL,Number(process.env.PORT),options).then(res=>message.channel.send(`MC server version = ${res.version.name}, number of players online = ${res.players.online} and latency = ${res.roundTripLatency}`)).catch(err=>message.channel.send(`Sorry, couldn't connect to the server. Here's the error I got: ${err} `))
     }
 
 })
